802d94658c0cbdf866cb2e86b8926458593d0a48
miguel <golindanomejias@gmail.com>
2024-12-04 07:25:36 -0400

    add function to make possible to play 5 rounds

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let roundTimes = 0;
 
 // this function let the computer choice using a random number
 function getComputerChoice() {
   let b = Math.floor(Math.random() * 100)
+  if (b <= 33) {
+    b = 'rock';
+  }
+
+  else if (b >= 66) {
+    b = 'paper';
+  }
+
+  else {
+    b = 'scissor';
+  }
+
   return b
 }
+
+
 
 //this function get the choice of the user and return it lowercase
 function getHumanChoice() {
@@ -16,59 +26,94 @@ function getHumanChoice() {
   return a.toLowerCase()
 }
 
-function playRound(humanChoice, computerChoice) {
-  
-  switch(humanChoice) {
 
-    case 'paper':      
-      if (computerChoice == 'paper') {
-        console.log('It\'s a draw!');
-      }
-      else if (computerChoice == 'rock') {
-        console.log('You win : Paper beats Rock!');
-        humanScore++
-      }
-
-      else if (computerChoice == 'scissor') {
-        console.log('You lose : Scissor beats Paper!');
-        computerScore++
-      }
-      break;
-  
-    case 'rock':
-      if (computerChoice == 'paper') {
-        console.log('You lose : Scissor beats Paper!');
-        computerScore++
-      }
-      else if (computerChoice == 'rock') {
-        console.log('It\'s a draw!');
-      }
-
-      else if (computerChoice == 'scissor') {
-        console.log('You win : Paper beats Rock!');
-        humanScore++
-      }
-      break;
-
-    case 'scissor':
-      if (computerChoice == 'paper') {
-        console.log('You win : Paper beats Rock!');
-        humanScore++
-      }
+for(let i = 0; i <= 5; i++) {
       
-      else if (computerChoice == 'rock') {
-        console.log('You lose : Scissor beats Paper!');
-        computerScore++        
-      }
+  playGame();
+  
+  if (roundTimes >= 4) {
+    console.log('Max Number of round, BYE :P')
+  }
 
-      else if (computerChoice == 'scissor') {
-        console.log('It\'s a draw!');
-      }
+  roundTimes++
 
-      break;
+}
+
+function playGame() {
+  
+  let humanScore = 0;
+  let computerScore = 0;
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+
+  function playRound(humanChoice, computerChoice) {
+
+    switch(humanChoice) {
+    
+      case 'paper':      
+        if (computerChoice == 'paper') {
+          console.log('It\'s a draw!');
+        }
+        else if (computerChoice == 'rock') {
+          console.log('You win : Paper beats Rock!');
+          humanScore++
+        }
+  
+        else if (computerChoice == 'scissor') {
+          console.log('You lose : Scissor beats Paper!');
+          computerScore++
+        }
+        break;
+    
+      case 'rock':
+        if (computerChoice == 'paper') {
+          console.log('You lose : Scissor beats Paper!');
+          computerScore++
+        }
+        else if (computerChoice == 'rock') {
+          console.log('It\'s a draw!');
+        }
+  
+        else if (computerChoice == 'scissor') {
+          console.log('You win : Paper beats Rock!');
+          humanScore++
+        }
+        break;
+  
+      case 'scissor':
+        if (computerChoice == 'paper') {
+          console.log('You win : Paper beats Rock!');
+          humanScore++
+        }
+        
+        else if (computerChoice == 'rock') {
+          console.log('You lose : Scissor beats Paper!');
+          computerScore++        
+        }
+  
+        else if (computerChoice == 'scissor') {
+          console.log('It\'s a draw!');
+        }
+
+        break;
+      
+      default:
+        console.log('Check your spell');
+    }
+
   }
 
 }
+
+
+
+
+
+
+
+
 
 /*
 console.log(getComputerChoice());
