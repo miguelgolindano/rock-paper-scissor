ed0c487822c212d31d15d0c38cd80952fc6d767e
miguel <golindanomejias@gmail.com>
2024-12-16 19:12:34 -0400

    adding three buttons using an event delegating tecnique to not use too many event listeners, this work thanks to the bubbling effect on the parent elements in this case the div with the id buttons

--- script.js
+++ script.js
@@ -20,26 +20,32 @@ function getComputerChoice() {
   return b
 }
 
+// this event listener uses the event delegation to listen for all the buttons
 
+let buttons = document.querySelector('#buttons');
+buttons.addEventListener('click', (event) => {
+  let target = event.target;
 
-//this function get the choice of the user and return it lowercase
-function getHumanChoice() {
-  let a = prompt('Your Choice', 'Here you can enter your choice')
-  return a.toLowerCase()
-}
+  switch(target.id) {
 
+    case 'buttonRock':
+      playRound('rock', playGame());
+      break
 
-for(let i = 0; i <= 4; i++) {
-      
-  playGame();
-  
-  if (roundTimes >= 4) {
-    console.log('Max Number of round, BYE :P')
+    case 'buttonPaper':
+      playRound('paper', playGame());
+      break
+
+    case 'buttonScissor':
+      playRound('scissor', playGame());
+      break
+
   }
 
-  roundTimes++
+});
 
-}
+
+
 
 function printScores() {
   console.log(`this is your score: ${humanScore}`) 
@@ -49,82 +55,78 @@ function printScores() {
 
 // playGame let the program compare results and choose a winner
 function playGame() {
-  
-  const humanSelection = getHumanChoice();
   const computerSelection = getComputerChoice();
-  
-  playRound(humanSelection, computerSelection);
+  return computerSelection
+}
 
-  /* this function use a switch and let select the winner comparing the humanChoice 
-    against the computerChoice and add a score on the winner counter */
-  function playRound(humanChoice, computerChoice) {
-    
-    /* this switch let select the winner comparing the humanChoice 
-    against the computerChoice and add a score on the winner counter */
+/* this function use a switch and let select the winner comparing the humanChoice 
+against the computerChoice and add a score on the winner counter */
+function playRound(humanChoice, computerChoice) {
 
-    switch(humanChoice) {
-    
-      case 'paper':      
-        if (computerChoice == 'paper') {
-          console.log('It\'s a draw!');
-          printScores()
-        }
-        else if (computerChoice == 'rock') {
-          console.log('You win : Paper beats Rock!');
-          humanScore++
-          printScores() 
-        }
-  
-        else if (computerChoice == 'scissor') {
-          console.log('You lose : Scissor beats Paper!');
-          computerScore++
-          printScores()
-        }
-        break;
-    
-      case 'rock':
-        if (computerChoice == 'paper') {
-          console.log('You lose : Scissor beats Paper!');
-          computerScore++
-          printScores() 
-        }
-        else if (computerChoice == 'rock') {
-          console.log('It\'s a draw!');
-          printScores()
-        }
-  
-        else if (computerChoice == 'scissor') {
-          console.log('You win : Paper beats Rock!');
-          humanScore++
-          printScores() 
-        }
-        break;
-  
-      case 'scissor':
-        if (computerChoice == 'paper') {
-          console.log('You win : Paper beats Rock!');
-          humanScore++
-          printScores() 
-        }
-        
-        else if (computerChoice == 'rock') {
-          console.log('You lose : Scissor beats Paper!');
-          computerScore++
-          printScores()    
-        }
-  
-        else if (computerChoice == 'scissor') {
-          console.log('It\'s a draw!');
-          printScores()
-        }
+  /* this switch let select the winner comparing the humanChoice 
+  against the computerChoice and add a score on the winner counter */
 
-        break;
+  switch(humanChoice) {
+  
+    case 'paper':      
+      if (computerChoice == 'paper') {
+        console.log('It\'s a draw!');
+        printScores()
+      }
+      else if (computerChoice == 'rock') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+        printScores() 
+      }
+
+      else if (computerChoice == 'scissor') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++
+        printScores()
+      }
+      break;
+  
+    case 'rock':
+      if (computerChoice == 'paper') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++
+        printScores() 
+      }
+      else if (computerChoice == 'rock') {
+        console.log('It\'s a draw!');
+        printScores()
+      }
+
+      else if (computerChoice == 'scissor') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+        printScores() 
+      }
+      break;
+
+    case 'scissor':
+      if (computerChoice == 'paper') {
+        console.log('You win : Paper beats Rock!');
+        humanScore++
+        printScores() 
+      }
       
-      default:
-        console.log('Check your spell');
-        console.log('-----------------------------------')
-    }
+      else if (computerChoice == 'rock') {
+        console.log('You lose : Scissor beats Paper!');
+        computerScore++
+        printScores()    
+      }
 
+      else if (computerChoice == 'scissor') {
+        console.log('It\'s a draw!');
+        printScores()
+      }
+
+      break;
+    
+    default:
+      console.log('Check your spell');
+      console.log('-----------------------------------')
   }
 
 }
