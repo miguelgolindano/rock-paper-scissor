beb7d9cd57def3a97b144867fb7bf90cccd08674
miguel <golindanomejias@gmail.com>
2024-12-18 13:17:42 -0400

    add a function that check the winner and display it in the document

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function getComputerChoice() {
   return b
 }
 
-// this event listener uses the event delegation to listen for all the buttons
 
+// this event listener uses the event delegation to listen for all the buttons
 let buttons = document.querySelector('#buttons');
 buttons.addEventListener('click', (event) => {
   let target = event.target;
@@ -30,24 +30,84 @@ buttons.addEventListener('click', (event) => {
 
     case 'buttonRock':
       playRound('rock', playGame());
+      checkWinner()
       break
 
     case 'buttonPaper':
       playRound('paper', playGame());
+      checkWinner()
       break
 
     case 'buttonScissor':
       playRound('scissor', playGame());
+      checkWinner()
       break
 
   }
 
 });
 
+// this function check the winner and replace the previous p if there were a previous p
+function checkWinner() {
+  const showWinner = document.querySelector('#showWinner');
 
+  if (humanScore === 5) {
+    const showWinner = document.querySelector('#showWinner');
+    let newDivWinner = document.createElement('div');
+    let oldDivWinner = document.querySelector('#showWinner>div');
+
+    if(!oldDivWinner){
+      showWinner.appendChild(newDivWinner);
+      newDivWinner.textContent = 'You are the winner';
+  } 
+    else {
+      oldDivWinner.replaceWith(newDivWinner);
+      newDivWinner.textContent = 'You are the winner';
+  }
+    computerScore = 0;
+    humanScore = 0;
+  }
+  
+  else if (computerScore === 5) {
+    const showWinner = document.querySelector('#showWinner');
+    let newDivWinner = document.createElement('div');
+    let oldDivWinner = document.querySelector('#showWinner>div');
+
+  if(!oldDivWinner){
+    showWinner.appendChild(newDivWinner);
+    newDivWinner.textContent = 'The computer is the winner';
+} 
+  else {
+    oldDivWinner.replaceWith(newDivWinner);
+    newDivWinner.textContent = 'The computer is the winner';
+  }
+    computerScore = 0;
+    humanScore = 0;
+  }
+}
 
 
 function printScores() {
+/*
+  const showResults = document.querySelector('#showResults');
+  const results = document.createElement('p');
+  results.textContent = `Your score: ${humanScore} -- Computer score: ${computerScore}`;
+  showResults.appendChild(results);
+*/
+  let body = document.querySelector('#showResults');
+  let newDiv = document.createElement('div');
+  let oldDiv = document.querySelector('#showResults>div');
+
+  if(!oldDiv){
+      body.appendChild(newDiv);
+      newDiv.textContent = `Your score: ${humanScore} -- Computer score: ${computerScore}`;
+  } else {
+      oldDiv.replaceWith(newDiv);
+      newDiv.textContent = `Your score: ${humanScore} -- Computer score: ${computerScore}`;
+  }
+
+
+
   console.log(`this is your score: ${humanScore}`) 
   console.log(`this is the computer score: ${computerScore}`)
   console.log('-----------------------------------')
